5ada4a6aba6f41b06f71d3b0fa95b6797845a73c
JanisLacis5 <janislacis06@gmail.com>
2024-05-07 23:27:03 +0300

    feat: finish encode_data func

--- functions.py
+++ functions.py
@@ -1,18 +1,29 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
 
 # X -> numpy array of data
 # encoder -> data encoder, for example, LabelEncoder, OneHotEncoder etc.
-# cols -> needed if
+# cols -> list of columns to  encode
 def encode_data(X, *args, encoder, cols=[], col_range=()):
-    if cols:
+    X = X.copy()
+    if isinstance(encoder, OneHotEncoder):
         transformer = ColumnTransformer(
-            [("transformer", encoder, cols)], remainder="passthrough"
+            [("one_hot_enc", encoder, cols)], remainder="passthrough"
         )
-        return {
-            "X": np.array(transformer.fit_transform(X)),
-            "transformer": transformer,
-        }
+        X = np.array(transformer.fit_transform(X))
+        encoder = transformer
+
+    else:
+        if cols:
+            for col in cols:
+                X[:, col] = encoder.fit_transform(X[:, col])
+        else:
+            X = encoder.fit_transform(X)
+
+    return {
+        "X": X,
+        "encoder": encoder,
+    }
